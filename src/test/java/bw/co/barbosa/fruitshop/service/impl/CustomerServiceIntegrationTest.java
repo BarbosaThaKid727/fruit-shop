b16187233fecb9122f2205fc3d999f8c15d27009
BarbosaThaKid727 <bbsmatlhare@gmail.com>
2021-04-16 01:31:06 +0200

    Patch Customer implemented. closes #14

---
+++ src/test/java/bw/co/barbosa/fruitshop/service/impl/CustomerServiceIntegrationTest.java
@@ -0,0 +1,110 @@
+package bw.co.barbosa.fruitshop.service.impl;
+
+import bw.co.barbosa.fruitshop.api.v1.dto.CustomerDTO;
+import bw.co.barbosa.fruitshop.api.v1.mapper.CustomerMapper;
+import bw.co.barbosa.fruitshop.bootstrap.Bootstrap;
+import bw.co.barbosa.fruitshop.model.Customer;
+import bw.co.barbosa.fruitshop.repository.CategoryRepository;
+import bw.co.barbosa.fruitshop.repository.CustomerRepository;
+import bw.co.barbosa.fruitshop.service.CustomerService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Slf4j
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class CustomerServiceIntegrationTest {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    CustomerService customerService;
+
+    CustomerMapper customerMapper = CustomerMapper.INSTANCE;
+
+    @BeforeEach
+    void setUp() throws Exception {
+
+        log.info("Load Customer Data");
+        log.info(String.valueOf(customerRepository.findAll().size()));
+
+        //setup data for testing
+        Bootstrap bootstrap = new Bootstrap(categoryRepository, customerRepository);
+        bootstrap.run(); //load data
+
+        customerService = new CustomerServiceImpl(customerMapper, customerRepository);
+    }
+
+    @Test
+    void patchCustomerUpdateFirstNameTest() throws Exception {
+
+        String updateName = "UpdateName";
+        Long id = getCustomerIdValue();
+
+        Customer customer = customerRepository.getOne(id);
+        assertNotNull(customer);
+        String customerFirstName = customer.getFirstname();
+        String customerLastName = customer.getLastname();
+
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setFirstname(updateName);
+
+        customerService.patchCustomer(id, customerDTO);
+
+        Customer updatedCustomer = customerRepository.findById(id).get();
+
+        assertNotNull(updatedCustomer);
+        assertEquals(updateName, updatedCustomer.getFirstname());
+        assertThat(customerFirstName, not(equalTo(updatedCustomer.getFirstname())));
+        assertThat(customerLastName, equalTo(updatedCustomer.getLastname()));
+    }
+
+    @Test
+    void patchCustomerUpdateLAstNameTest() throws Exception {
+
+        String updatedName = "UpdatedName";
+        long id = getCustomerIdValue();
+
+        Customer originalCustomer = customerRepository.getOne(id);
+        assertNotNull(originalCustomer);
+
+        //save original first/last name
+        String originalFirstName = originalCustomer.getFirstname();
+        String originalLastName = originalCustomer.getLastname();
+
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setLastname(updatedName);
+
+        customerService.patchCustomer(id, customerDTO);
+
+        Customer updatedCustomer = customerRepository.findById(id).get();
+
+        assertNotNull(updatedCustomer);
+        assertEquals(updatedName, updatedCustomer.getLastname());
+        assertThat(originalFirstName, equalTo(updatedCustomer.getFirstname()));
+        assertThat(originalLastName, not(equalTo(updatedCustomer.getLastname())));
+    }
+
+    private Long getCustomerIdValue() {
+
+        List<Customer> customers = customerRepository.findAll();
+        log.info("Customers found " + customers.size());
+        return customers.get(0).getId();
+    }
+}
